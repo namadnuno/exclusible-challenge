d419f1f46b61c9f53afdad5fa87e1c405a5e36ea
Nuno Alexandre <nunnomalex@gmail.com>
2022-04-14 13:49:13 +0100

    feat: setup expressjs server

---
+++ api/config.ts
@@ -0,0 +1,5 @@
+const Config = {
+    API_PORT: process.env.API_PORT as string,
+}
+
+export default Config;
--- api/server.ts
+++ api/server.ts
@@ -1,1 +1,14 @@
-// TODO: API up and running
+import express from 'express';
+import Config from './config';
+
+const app = express()
+
+const port = Config.API_PORT;
+
+app.get('/', (_req, res) => {
+  res.send('"ONE SMALL STEP FOR MAN. ONE GIANT LEAP FOR MANKIND."')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
